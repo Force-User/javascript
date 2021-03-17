1513f42103a48c6346f2ecf646da6f3b59eca3c7
Anatoly Yakyshenko <anatoly.yakyshenko@gmail.com>
2021-03-17 08:37:00 +0300

    refactor: refactor script.js
    - moved checks to separate functions;
    - only numbers values ​​are stored in the cache object;
    - added (try catch) to findSum function;

--- 3. sum of row/script.js
+++ 3. sum of row/script.js
@@ -1,34 +1,49 @@
 const findeMinMaxSum = findSum();
 console.group();
-console.log(findeMinMaxSum(10, 2));
-console.log(findeMinMaxSum(10, "5"));
-console.log(findeMinMaxSum(10, 5));
-console.log(findeMinMaxSum(10, Infinity));
-console.log(findeMinMaxSum(10, "12w"));
-console.log(findeMinMaxSum(100, 1));
+console.log("1. (10, 2) ", findeMinMaxSum(10, 2));
+console.log("2. (1, '5')", findeMinMaxSum(1, "5"));
+console.log("3. (1, 5)", findeMinMaxSum(1, 5));
+console.log("4. (1, Infinity)", findeMinMaxSum(1, Infinity));
+console.log("5. (1, '12w')", findeMinMaxSum(1, "12w"));
+console.log("6. (1, {})", findeMinMaxSum(1, {}));
+console.log("7. ({}, 1)", findeMinMaxSum({}, 1));
+console.log("8. (true, false)", findeMinMaxSum(true, false));
+console.log("9. (false, true)", findeMinMaxSum(false, true));
+console.log("10. (1, 9007199254740992)", findeMinMaxSum(1, 9007199254740992));
+console.log("11. (1, 10071)", findeMinMaxSum(1, 10071));
+console.log("12. (-10, -1)", findeMinMaxSum(-10, -1));
+console.log("13. (-1, 0)", findeMinMaxSum(-1, 0));
+console.log("14. (1, 100)", findeMinMaxSum(1, 100));
 console.groupEnd();
 
 function findSum() {
   const cache = {};
 
   return (min, max) => {
-    if (min > max) {
-      [min, max] = [max, min];
-    }
-    const stringArguments = `${min} ${max}`;
-    if (!cache[stringArguments]) {
-      cache[stringArguments] = (Number(min) + Number(max)) * (max / 2);
-    }
-
-    switch (true) {
-      case cache[stringArguments] > Number.MAX_SAFE_INTEGER:
-        console.error("The maximum integer number has been exceeded");
-        break;
-      case Number.isNaN(cache[stringArguments]):
-        console.error("Invalid number");
-        break;
-      default:
-        return cache[stringArguments];
+    try {
+      checkValue(min, max);
+      const stringArguments = `${min} ${max}`;
+      if (!cache[stringArguments]) {
+        cache[stringArguments] = (min + max) * (max / 2);
+      }
+      isIntegerSafe(cache[stringArguments]);
+      return cache[stringArguments];
+    } catch (e) {
+      console.error(e);
     }
   };
+}
+
+function checkValue(min, max) {
+  if (min > max) {
+    throw "The minimum value is greater than the maximum";
+  } else if (!Number.isFinite(min) || !Number.isFinite(max)) {
+    throw "Invalid Value";
+  }
+}
+
+function isIntegerSafe(value) {
+  if (value > Number.MAX_SAFE_INTEGER) {
+    throw "The maximum iteger number has been exceeded";
+  }
 }
