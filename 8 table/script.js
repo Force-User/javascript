836984d7f9d29b76c9690c28b75c0820df24dd03
Anatoly Yakyshenko <anatoly.yakyshenko@gmail.com>
2021-04-27 11:23:28 +0300

    reafactor: refactor hw8
    - refactor fillTable
    - refactor sorting
    -refactor clearTable

--- 8 table/script.js
+++ 8 table/script.js
@@ -10,11 +10,13 @@ const createRow = (data, count) => {
   nameCell.classList.add("table-row-name");
   nameCell.dataset.id = data.id;
   nameCell.innerText = data.name;
+  nameCell.dataset.name = "name";
   nameCell.setAttribute("contenteditable", true);
 
   const positionCell = document.createElement("td");
   positionCell.classList.add("table-row-position");
   positionCell.dataset.id = data.id;
+  positionCell.dataset.name = "position";
   positionCell.innerText = data.position;
   positionCell.setAttribute("contenteditable", true);
 
@@ -23,12 +25,16 @@ const createRow = (data, count) => {
 };
 
 const clearTable = () => {
-  const allRows = document.querySelectorAll(".table-row");
-  allRows.forEach((item) => item.remove());
+  [...table.children].forEach(
+    (child) =>
+      child.classList.contains("table-row") &&
+      table.removeChild(table.lastChild)
+  );
 };
 
 const fillTable = (data) => {
-  data.forEach((item, index) => table.append(createRow(item, index + 1)));
+  const rows = data.map((item, index) => createRow(item, index + 1));
+  table.append(...rows);
 };
 
 const sortDescending = (data, prop) => {
@@ -82,8 +88,8 @@ const data = [
 ];
 
 const stateSorting = {
-  name: false,
-  position: false,
+  toName: false,
+  toPosition: false,
 };
 
 const table = document.querySelector(".table");
@@ -95,12 +101,7 @@ const tableRows = document.querySelectorAll(".table-row-name");
 tableHeader.addEventListener("click", (e) => {
   const element = e.target.closest("td");
   if (!element) return;
-
-  if (element.classList.contains("table-header-name")) {
-    return sorting(data, "name");
-  } else if (element.classList.contains("table-header-position")) {
-    return sorting(data, "position");
-  }
+  return sorting(data, element.dataset.sort);
 });
 
 table.addEventListener(
@@ -108,12 +109,11 @@ table.addEventListener(
   (e) => {
     const element = e.target.closest("td");
     if (!element) return;
-
-    if (element.classList.contains("table-row-name")) {
-      return setNewInfo(e.target.innerText, e.target.dataset.id, "name");
-    } else if (element.classList.contains("table-row-position")) {
-      return setNewInfo(e.target.innerText, e.target.dataset.id, "position");
-    }
+    return setNewInfo(
+      element.innerText,
+      element.dataset.id,
+      element.dataset.name
+    );
   },
   true
 );
